59b496c48f42c6b3312fef8728a2f62afc5c67f4
Sushant Chougule <sushantchougule0206@gmail.com>
2023-08-07 15:12:30 +0530

    Developers story

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ def addition(a,b,c):
     print(a+b+c)
     return a+b+c
 
-def multiplication(a,b,c):
-    return a*b*c
+def multiplication(a,b):
+    return a*b
+
+def divition(a,b):
+    return a/b
